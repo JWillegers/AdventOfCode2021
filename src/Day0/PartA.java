a142e3825a6f8f4f3e843bb3d62ad3cf8425aa5a
Jonathan <148167.jw@gmail.com>
2021-11-24 16:10:59 +0100

    Day0 (template)

---
+++ src/Day0/PartA.java
@@ -0,0 +1,38 @@
+package Day0;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PartA {
+    private int nOfLines = 10;
+    private BufferedReader reader;
+    private String[] array = new String[nOfLines];
+
+    public static void main(String[] args) {
+        PartA part = new PartA();
+        part.setup();
+        part.solution();
+    }
+
+    public void setup() {
+        try {
+            reader = new BufferedReader(new FileReader("src/Day/input.txt")); //TODO change filepath
+            for (int i = 0; i < nOfLines; i++) {
+                array[i] = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void solution() {
+        for (int i = 0; i < nOfLines; i++) {
+
+        }
+
+        System.out.println();
+    }
+}

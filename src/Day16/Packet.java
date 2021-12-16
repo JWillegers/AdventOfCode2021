fba0c5195db3206685789ab6fbbb60b780499ca1
Jonathan <148167.jw@gmail.com>
2021-12-16 21:55:02 +0100

    Day16 Part1

--- src/Day16/Packet.java
+++ src/Day16/Packet.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class Packet {
     protected int version;
     protected int typeID;
-    protected int value;
+    protected long value;
     protected int operator;
+    protected int label;
     protected List<Packet> innerPackets;
 }

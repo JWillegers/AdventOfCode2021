75b10c7adaf1d6139eae32f49a2f11dc2bbf12ba
Jonathan <148167.jw@gmail.com>
2021-12-26 16:23:15 +0100

    Day22 WIP

--- src/Day22/PartB.java
+++ src/Day22/PartB.java
@@ -5,14 +5,15 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class PartB {
-    private final int nOfLines = 420;
-    private int regionX = 0;
-    private int shiftX = 0;
-    private int regionY = 0;
-    private int shiftY = 0;
-    private int regionZ = 0;
-    private int shiftZ = 0;
-    private String[] array = new String[nOfLines];
+    private final int nOfLines = 60;
+    private int minX;
+    private int maxX;
+    private int minY;
+    private int maxY;
+    private int minZ;
+    private int maxZ;
+    private final int step = 1000;
+    private int[][] array = new int[nOfLines][7];
     private boolean[][][] cubes;
 
     public static void main(String[] args) {
@@ -23,48 +24,40 @@ public class PartB {
 
     public void setup() {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/Day22/input.txt"));
-            int minX = 0;
-            int minY = 0;
-            int minZ = 0;
-            int maxX = 0;
-            int maxY = 0;
-            int maxZ = 0;
+            BufferedReader reader = new BufferedReader(new FileReader("src/Day22/test.txt"));
+            minX = 0;
+            maxX = 0;
+            minY = 0;
+            maxY = 0;
+            minZ = 0;
+            maxZ = 0;
             for (int i = 0; i < nOfLines; i++) {
-                array[i] = reader.readLine();
-                /*
-                [1] => min x
-                [3] => max x
-                [5] => min y
-                [7] => max y
-                [9] => min z
-                [11] => max z
-             */
-                String[] split = array[i].split("[,=.]");
-                if (Integer.parseInt(split[1]) < minX) {
-                    minX = Integer.parseInt(split[1]);
-                } else if (Integer.parseInt(split[3]) > maxX) {
-                    maxX = Integer.parseInt(split[3]);
+                String line = reader.readLine();
+                String[] split = line.split("[,=.]");
+                array[i][0] = split[0].contains("on") ? 0 : 1;
+                array[i][1] = Integer.parseInt(split[1]);
+                array[i][2] = Integer.parseInt(split[3]);
+                array[i][3] = Integer.parseInt(split[5]);
+                array[i][4] = Integer.parseInt(split[7]);
+                array[i][5] = Integer.parseInt(split[9]);
+                array[i][6] = Integer.parseInt(split[11]);
+                if (array[i][1] < minX) {
+                    minX = array[i][1];
+                } else if (array[i][2] > maxX) {
+                    maxX = array[i][2];
                 }
-                if (Integer.parseInt(split[5]) < minY) {
-                    minY = Integer.parseInt(split[5]);
-                } else if (Integer.parseInt(split[7]) > maxY) {
-                    maxY = Integer.parseInt(split[7]);
+                if (array[i][3] < minY) {
+                    minY = array[i][3];
+                } else if (array[i][4] > maxY) {
+                    maxY = array[i][4];
                 }
-                if (Integer.parseInt(split[9]) < minZ) {
-                    minZ = Integer.parseInt(split[9]);
-                } else if (Integer.parseInt(split[11]) > maxZ) {
-                    maxZ = Integer.parseInt(split[11]);
+                if (array[i][5] < minZ) {
+                    minZ = array[i][5];
+                } else if (array[i][6] > maxZ) {
+                    maxZ = array[i][6];
                 }
             }
             reader.close();
-            shiftX = -minX;
-            regionX = shiftX + maxX + 1;
-            shiftY = -minY;
-            regionY = shiftY + maxY + 1;
-            shiftZ = -minZ;
-            regionZ = shiftZ + maxZ + 1;
-            cubes = new boolean[regionX][regionY][regionZ];
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
@@ -72,37 +65,63 @@ public class PartB {
     }
 
     public void solution() {
-        for (int i = 0; i < nOfLines; i++) {
-            /*
-                [1] => min x
-                [3] => max x
-                [5] => min y
-                [7] => max y
-                [9] => min z
-                [11] => max z
-             */
-            String[] split = array[i].split("[,=.]");
-            for (int x = Integer.parseInt(split[1]); x <= Integer.parseInt(split[3]); x++) {
-                for (int y = Integer.parseInt(split[5]); y <= Integer.parseInt(split[7]); y++) {
-                    for (int z = Integer.parseInt(split[9]); z <= Integer.parseInt(split[11]); z++) {
-                        cubes[x + shiftX][y + shiftY][z + shiftZ] = split[0].contains("on");
+        long counter = 0;
+        cubes = new boolean[step][step][step];
+        for (int direction1 = minX; direction1 <= maxX; direction1+= step) {
+            for (int direction2 = minY; direction2 <= maxY; direction2 += step) {
+                System.out.println("xy: " + direction1 + ", " + direction2 + "; count: " + counter);
+                boolean on = false;
+                for (int direction3 = minZ; direction3 <= maxZ; direction3 += step) {
+                    boolean tryCount = false;
+                    for (int i = 0; i < nOfLines; i++) {
+                    /*
+                        [1] => min x
+                        [3] => max x
+                        [5] => min y
+                        [7] => max y
+                        [9] => min z
+                        [11] => max z
+                     */
+                        if (array[i][0] == 1 || (array[i][0] == 0 && on)) {
+                            for (int x = Math.max(array[i][1], direction1); x < Math.min(array[i][2], direction1 + step); x++) {
+                                for (int y = Math.max(array[i][3], direction2); y < Math.min(array[i][4], direction2 + step); y++) {
+                                    for (int z = Math.max(array[i][5], direction3); z < Math.min(array[i][6], direction3 + step); z++) {
+                                        try {
+                                            cubes[x - direction1][y - direction2][z - direction3] = array[i][0] == 1;
+                                            on = true;
+                                        } catch (IndexOutOfBoundsException e) {
+                                            e.printStackTrace();
+                                            System.out.println("x: " + x);
+                                            System.out.println(x + Math.abs(direction1));
+                                            System.out.println("y: " + y);
+                                            System.out.println(y + Math.abs(direction2));
+                                            System.out.println("z: " + z);
+                                            System.out.println(z + Math.abs(direction3));
+                                            System.out.println(array[i][6]);
+                                            System.out.println(direction3);
+                                            System.exit(1);
+                                        }
+                                        tryCount = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (tryCount) {
+                        for (int x = 0; x < step; x++) {
+                            for (int y = 0; y < step; y++) {
+                                for (int z = 0; z < step; z++) {
+                                    if (cubes[x][y][z]) {
+                                        counter++;
+                                        cubes[x][y][z] = false;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
-
         }
-
-        int counter = 0;
-        for (int x = 0; x < regionX; x++) {
-            for (int y = 0; y < regionY; y++) {
-                for (int z = 0; z < regionZ; z++) {
-                    if (cubes[x][y][z]) {
-                        counter++;
-                    }
-                }
-            }
-        }
-
         System.out.println(counter);
     }
 }

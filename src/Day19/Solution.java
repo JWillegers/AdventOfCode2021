c889e5282f893255c1fe24cbf875dbc8bbe40a83
Jonathan <148167.jw@gmail.com>
2022-04-22 13:45:29 +0200

    Day19 WIP rotations

--- src/Day19/Solution.java
+++ src/Day19/Solution.java
@@ -61,47 +61,82 @@ public class Solution {
         scannerZero.cord.y = 0;
         scannerZero.cord.z = 0;
 
-        Scanner scannerOne = myScanners.get(1);
-        for (int i = 0; i < scannerZero.beacons.size(); i++) {
-            Beacon b0 = scannerZero.beacons.get(i);
-            List<Cord> r0 = b0.relativePositionsOtherBeacons;
-            for(int j = 0; j < scannerOne.beacons.size(); j++) {
-                Beacon b1 = scannerOne.beacons.get(j);
-                List<Cord> r1 = b1.relativePositionsOtherBeacons;
-                int matches = 0;
-                for (Cord c0 : r0) {
-                    for(Cord c1 : r1) {
-                        if(b0.cord.x == 0) {
-                            System.out.println(c0.x + " " + c0.y + " " + c0.z
-                                    + " " + c1.x + " " + c1.y + " " + c1.z);
-                        }
-                        if(c0.x==-c1.x && c0.y==c1.y && c0.z==-c1.z) {
-                            matches++;
+        boolean run = true;
+        boolean match = false;
+
+        Scanner s_a = myScanners.get(0);
+        while(!match) {
+            Scanner s_b = myScanners.get(1);
+            int i = 0;
+            while(i < s_a.beacons.size() && !match) {
+                Beacon b0 = s_a.beacons.get(i);
+                List<Cord> r0 = b0.relativePositionsOtherBeacons;
+                int j = 0;
+                while(j < s_b.beacons.size() && !match) {
+                    Beacon b1 = s_b.beacons.get(j);
+                    List<Cord> r1 = b1.relativePositionsOtherBeacons;
+                    for(int mx = -1; mx < 2; mx += 2) {
+                        for(int my = -1; my < 2; my += 2) {
+                            for(int mz = -1; mz < 2; mz += 2) {
+                                int face = 0;
+                                while(face < 6 && !match) {
+                                    /*
+                                    0: x,y,z
+                                    1: x,z,y
+                                    2: y,x,z
+                                    3: y,z,x
+                                    4: z,x,y
+                                    5: z,y,x
+                                     */
+                                    int matches = 0;
+                                    for (Cord c0 : r0) {
+                                        for (Cord c1 : r1) {
+                                            if(face == 0) {
+                                                if (c0.x == mx * c1.x && c0.y == my * c1.y && c0.z == mz * c1.z) {
+                                                    matches++;
+                                                }
+                                            }
+                                            else if(face == 1) {
+                                                if (c0.x == mx * c1.x && c0.z == my * c1.y && c0.y == mz * c1.z) {
+                                                    matches++;
+                                                }
+                                            }
+                                            else if(face == 2) {
+                                                if (c0.y == mx * c1.x && c0.x == my * c1.y && c0.z == mz * c1.z) {
+                                                    matches++;
+                                                }
+                                            }
+                                            else if(face == 3) {
+                                                if (c0.y == mx * c1.x && c0.z == my * c1.y && c0.x == mz * c1.z) {
+                                                    matches++;
+                                                }
+                                            }
+                                            else if(face == 4) {
+                                                if (c0.z == mx * c1.x && c0.x == my * c1.y && c0.y == mz * c1.z) {
+                                                    matches++;
+                                                }
+                                            }
+                                            else if (c0.z == mx * c1.y && c0.z == my * c1.y && c0.x == mz * c1.z) {
+                                                matches++;
+                                            }
+                                        }
+                                    }
+                                    if (matches >= 12) {
+                                        s_b.cord.x = s_a.cord.x + b0.cord.x + -mx * b1.cord.x;
+                                        s_b.cord.y = s_a.cord.y + b0.cord.y + -my * b1.cord.y;
+                                        s_b.cord.z = s_a.cord.z + b0.cord.z + -mz * b1.cord.z;
+                                        s_b.matched = true;
+                                        match = true;
+                                    }
+                                    face++;
+                                }
+                            }
                         }
                     }
+                    j++;
                 }
-                if (matches >= 12) {
-                    System.out.println("=====================");
-                    System.out.println(b0.cord.x + " " + b0.cord.y + " " + b0.cord.z);
-                    System.out.println(b1.cord.x + " " + b1.cord.y + " " + b1.cord.z);
-                    scannerOne.cord.x = scannerZero.cord.x + b0.cord.x + b1.cord.x;
-                    scannerOne.cord.y = scannerZero.cord.y + b0.cord.y + -b1.cord.y;
-                    scannerOne.cord.z = scannerZero.cord.z + b0.cord.z + b1.cord.z;
-                    System.out.println(scannerOne.cord.x + ", " + scannerOne.cord.y + ", " + scannerOne.cord.z);
-                    return;
-                }
-
-                if(b0.cord.x == 0) {
-                    System.out.println("=x=x=x=x=x=x=x=x=x=x=");
-                    System.out.println(b1.cord.x + " " + b1.cord.y + " " + b1.cord.z
-                            + " " + matches);
-                    System.out.println("=x=x=x=x=x=x=x=x=x=x=");
-                }
-
-
-
+                i++;
             }
         }
-
     }
 }

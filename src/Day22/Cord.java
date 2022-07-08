edd338d56ed47c43607c078df123483f75e8df64
Jonathan <148167.jw@gmail.com>
2022-07-08 11:48:17 +0200

    DAY22 Clean version for new idea

--- src/Day22/Cord.java
+++ src/Day22/Cord.java
@@ -4,16 +4,10 @@ public class Cord {
     protected int x;
     protected int y;
     protected int z;
-    protected int xLine;
-    protected int yLine;
-    protected int zLine;
 
     protected Cord(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.xLine = 10;
-        this.yLine = 10;
-        this.zLine = 10;
     }
 }
